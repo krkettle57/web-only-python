5210daa9f383e8878fe2701967b7a9736915a15d
krkettle57 <krkettle57@gmail.com>
2022-05-14 11:13:13 +0900

    ユーザクライアントのIFを修正

--- yaoya/services/user.py
+++ yaoya/services/user.py
@@ -1,25 +1,41 @@
-from typing import Optional, Protocol
+from typing import Protocol
 
 import dataset
+from tinydb import Query
 from yaoya.models.user import User
-from yaoya.services.mock import MockDB
+from yaoya.services.mock import MockDB, MockSessionDB
+
+from services.base import NotFoundError
 
 
 class IUserAPIClientService(Protocol):
-    def login(self, user_id: str, password: str) -> Optional[User]:
+    def get_by_session_id(self, session_id: str) -> User:
         pass
 
 
 class MockUserAPIClientService(IUserAPIClientService):
-    def __init__(self, mockdb: MockDB) -> None:
+    def __init__(self, mockdb: MockDB, session_db: MockSessionDB) -> None:
         self.mockdb = mockdb
+        self.session_db = session_db
 
-    def login(self, user_id: str, password: str) -> Optional[User]:
+    def get_by_session_id(self, session_id: str) -> User:
+        user_id = self._get_user_id(session_id)
+        user = self._get_by_user_id(user_id)
+        return user
+
+    def _get_user_id(self, session_id: str) -> str:
+        with self.session_db.connect() as db:
+            query = Query()
+            doc = db.search(query.session_id == session_id)
+
+        return doc[0]["user_id"]
+
+    def _get_by_user_id(self, user_id: str) -> User:
         with self.mockdb.connect() as db:
             table: dataset.Table = db["users"]
             user_data = table.find_one(user_id=user_id)
 
         if user_data is None:
-            return None
+            raise NotFoundError()
 
         return User.from_dict(user_data)
